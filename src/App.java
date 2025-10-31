711143618545a308baa631bae7678c46104b6a40
mhelve46 <mhelve46@students.byron.k12.mn.us>
2025-10-31 09:15:53 -0500

    commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Create a variable to store our team number
+
+        // Create a variable to store our team name
+
+        // Create a variable to store our robot name
+
+        // Create a variable to store the current match time
+
+        // Create a variable to store whether our robot is ready to climb
+        // Hint (true/false)
+
+        /* if the current match time is past 67.5 seconds,
+         * and the robot is ready to climb, 
+         * 
+         * then print a statement or set of statements listing acknowleding our climbing
+         * include robot name, team name, and team number variables
+         * 
+         * if either condition is not met, then print a statement declaring the reason.
+         */
+    }
+}
---
+++ src/Debug1.java
@@ -0,0 +1,52 @@
+public class Debug1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to the Arena!");
+        
+        /* 
+         * code should run with no errors
+         * 
+         * the print statement inside the if statement should run
+         * 
+         * the second print statement should say 65
+         * 
+         * the third should say 65.5
+         */
+
+
+
+
+
+        // Store our squad code (should be 3.0)
+        int percentOfMatchesWon = 3/0;
+        
+        // Store our squad title
+        String 4859team == "The Cybears (lowercase b)";
+
+        // Store our machine alias
+        string machineAlias = 'Insomnolence';
+
+        // Store the seconds left in the round
+        int secondsLeft = 65.0
+
+        Store if our machine is powered up for defense
+        // Hint: true or false
+        String defenseReady = "false";
+        
+        // Check if defenseReady is false and print Minecraft sleep quote
+        if (defenseReady = false) {
+            System.out.println("You may not rest now, [" + machinealias + "] is nearby");
+        }
+        
+        // this line of code does not need to change. 
+        TreeMap<String, Integer> map = new TreeMap<String, Integer>();
+
+        // should print 65
+        System.out.println(secondsLeft);
+
+        secondsLeft += .5;
+        
+        // should print 65.5
+        System.out.println(secondsLeft);
+
+    }
+}
